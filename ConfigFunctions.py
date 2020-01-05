2d3712a8b043dcd7aa66a1dbbe1af7c9f95f675d
Killian264 <killiandebacker@gmail.com>
2020-01-05 00:28:09 -0600

    small changes and comments

---
+++ ConfigFunctions.py
@@ -0,0 +1,34 @@
+import json
+
+configLoc = "./config.json"
+baseMusicLoc = "./Music/"
+
+# this could be changed out later to some kind of object or have an object passed after which it would be converted and written to file
+def updateConfig(volume, musicFolder, configLoc=configLoc):
+    config ={
+        "Volume": volume,
+        "MusicFolder": musicFolder
+    }
+    with open(configLoc, "w") as file:
+        json.dump(config, file)
+
+
+def getSettings(configLoc=configLoc):
+    # This can be updated later with more settings pretty easily
+    try:
+        with open(configLoc) as file:
+            config = json.load(file)
+    except:
+        updateConfig(10, baseMusicLoc)
+        return 10, baseMusicLoc
+
+    volume = config["Volume"]
+    musicLoc = config["MusicFolder"]
+
+    if volume == '' or musicLoc == '':
+        volume = volume == '' if 10 else volume
+        musicLoc = musicLoc == '' if baseMusicLoc else musicLoc
+
+        updateConfig(volume, musicLoc)
+
+    return volume, musicLoc
--- PyMusic.py
+++ PyMusic.py
@@ -3,25 +3,35 @@ import icons_rc
 from PyQt5.QtMultimedia import *
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 import sys
 import json
-
+import shutil
 configLoc = "./config.json"
+from ConfigFunctions import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.volume = 60 # This will be changed out later for a function that takes from config.json see bottem of setupUI for current implementation
+        # gets user settings
+        self.getSettings()
+
+        # setup play state
         self.playState = 0 # 0 is paused 1 is playing
+        # gets the playlist of songs from folder see func
         self.getPlaylist()
+        # player stuff
         self.player = QMediaPlayer()
         self.player.setPlaylist(self.playlist)
         self.player.setVolume(self.volume)
 
+        # setupUi is all stuff made in QtDesigner
         self.setupUi()
+        # connects basically setting functions to event handlers
         self.connects()
-        self.getSettings()
+        # very important
+        self.changeSpeakerImg()
 
 
         # thingies
@@ -34,14 +44,19 @@ class MainWindow(QMainWindow):
         # Audio Slider
         # Menubar
 
+    # gets settings and change object state this will probably be chagned later so its a function
     def getSettings(self):
-        # This can be updated later with more settings pretty easily
-        with open(configLoc) as file:
-            config = json.load(file)
+        # configLoc does not need to be passed
+        self.volume, self.musicLoc = getSettings(configLoc)
 
-        self.volume = config["Volume"]
-        self.musicLoc = config["MusicFolder"]
-        self.player.setVolume(self.volume)
+    # this will be changed out later for a more robust loader of multiple songs
+    def getPlaylist(self):
+        self.playlist = QMediaPlaylist()
+        url = QtCore.QUrl.fromLocalFile("./Music/TestSong.wav")
+        self.playlist.addMedia(QMediaContent(url))
+        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+        print(self.playlist.mediaCount())
+
 
     def setupUi(self):
         self.setObjectName("MainWindow")
@@ -121,20 +136,28 @@ class MainWindow(QMainWindow):
         self.forward.setObjectName("forward")
         self.horizontalLayout.addWidget(self.forward)
 
-        # Progress bar
-        self.songProgressBar = QtWidgets.QProgressBar(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.songProgressBar.sizePolicy().hasHeightForWidth())
-        self.songProgressBar.setSizePolicy(sizePolicy)
-        self.songProgressBar.setMaximumSize(QtCore.QSize(16777215, 2))
-        self.songProgressBar.setStyleSheet("QProgressBar::chunk { background: black; }")
-        self.songProgressBar.setProperty("value", 24)
-        self.songProgressBar.setAlignment(QtCore.Qt.AlignCenter)
-        self.songProgressBar.setFormat("")
-        self.songProgressBar.setObjectName("songProgressBar")
-        self.horizontalLayout.addWidget(self.songProgressBar)
+        # song progress slider
+        self.songSlider = QtWidgets.QSlider(self.centralwidget)
+        self.songSlider.setStyleSheet(".QSlider {\n"
+                                            "    min-height: 20px;\n"
+                                            "    max-height: 20px;\n"
+                                            "}\n"
+                                            "\n"
+                                            ".QSlider::groove:horizontal {\n"
+                                            "    height: 2px;\n"
+                                            "    background: #d1d1cf;\n"
+                                            "}\n"
+                                            "\n"
+                                            ".QSlider::handle:horizontal {\n"
+                                            "    background: white;\n"
+                                            "    width: 10px;\n"
+                                            "    height: 10px;\n"
+                                            "    margin: -1px 0px;\n"
+                                            "}")
+        self.songSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.songSlider.setObjectName("horizontalSlider")
+        self.horizontalLayout.addWidget(self.songSlider)
+
 
         # speaker image label
         self.speakerImage = QtWidgets.QLabel(self.centralwidget)
@@ -145,7 +168,7 @@ class MainWindow(QMainWindow):
         self.speakerImage.setSizePolicy(sizePolicy)
         self.speakerImage.setMaximumSize(QtCore.QSize(20, 20))
         self.speakerImage.setText("")
-        self.speakerImage.setPixmap(QtGui.QPixmap(":/icons/speaker.png"))
+        self.speakerImage.setPixmap(QtGui.QPixmap(":/icons/speaker-max.png"))
         self.speakerImage.setScaledContents(True)
         self.speakerImage.setObjectName("speakerImage")
         self.horizontalLayout.addWidget(self.speakerImage)
@@ -195,15 +218,11 @@ class MainWindow(QMainWindow):
         self.menuFile.addAction(self.changeDefaultFolder)
         self.menubar.addAction(self.menuFile.menuAction())
 
+        # not sure why retranslateUi is split up
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def updateProgressBar(self):
-        print((self.player.position() / self.player.duration()))
-        print((self.player.position() / self.player.duration()) * 100)
-        self.songProgressBar.setValue((self.player.position() / self.player.duration()) * 100)
-        QGuiApplication.processEvents()
-
+    # naming things? this was made by the QtDesigner
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("MainWindow", "PyMusic"))
@@ -211,46 +230,85 @@ class MainWindow(QMainWindow):
         self.fileMenuOpen.setText(_translate("MainWindow", "Open"))
         self.changeDefaultFolder.setText(_translate("MainWindow", "Change Music Folder"))
 
+    # basically event listeners for changes
     def connects(self):
         self.previous.clicked.connect(self.prevSong)
         self.play.clicked.connect(self.playSong)
         self.forward.clicked.connect(self.nextSong)
         self.audioSlider.valueChanged.connect(self.changeAudioVolume)
+        self.songSlider.sliderMoved.connect(self.changeSongPosition)
+        #self.songSlider.valueChanged.connect(self.changeSongPosition)
+        #self.player.positionChanged.connect(self.changeSliderPosition)
 
-    def prevSong(self):
-        print("play prev")
+        self.fileMenuOpen.triggered.connect(self.getFile)
+        self.changeDefaultFolder.triggered.connect(self.getDirectory)
 
+
+    # File Thingies
+    def getFile(self):
+        # open dialog, check if user has file, move file to music Folder
+        fileInfo = QFileDialog.getOpenFileName(self, 'Open File')
+        currentLoc, typeOrSomething = fileInfo
+        if not currentLoc == '':
+            shutil.move(currentLoc, self.musicLoc)
+
+    def getDirectory(self):
+        # open dialog, check if user has file, move file to music Folder
+        dir = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if not dir == '':
+            self.musicLoc = dir
+            updateConfig(self.volume, dir, configLoc)
+
+    # song slider change connects
+    # these don't work properly fix later
+    def changeSongPosition(self, position):
+        self.player.setPosition(position * (self.player.duration() / 100))
+
+    def changeSliderPosition(self, position):
+        self.songSlider.setSliderPosition(position / (self.player.duration() / 100))
+
+    # audio slider change connect
     def changeAudioVolume(self):
         self.volume = self.audioSlider.value()
         self.player.setVolume(self.volume)
+        self.changeSpeakerImg()
 
+    # changes speaker image based on volume
+    def changeSpeakerImg(self):
+        img = QtGui.QPixmap(":/icons/speaker-none.png")
+        if self.volume > 70:
+            img = QtGui.QPixmap(":/icons/speaker-max.png")
+        elif self.volume < 70 and self.volume > 30:
+            img = QtGui.QPixmap(":/icons/speaker-med.png")
+        elif self.volume < 30 and self.volume > 0:
+            img = QtGui.QPixmap(":/icons/speaker-low.png")
+        self.speakerImage.setText("")
+        self.speakerImage.setPixmap(img)
+
+    # Prev song // plan is to either load songs into array of file locations
+    def prevSong(self):
+        print("play prev")
+
+    def nextSong(self):
+        print("play next")
+
+    # checks song state and changes it
     def playSong(self):
         # stop may be added later
         if self.playState == 0:
             self.player.play()
             self.playState = 1
-            self.play.setIcon(self.makeIcon(":/icons/pause.png"))
-            self.updateProgressBar()
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap(":/icons/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.play.setIcon(icon)
         elif self.playState == 1:
             self.player.pause()
             self.playState = 0
-            self.play.setIcon(self.makeIcon(":/icons/Play.png"))
-    # switch this out later for some kind of self.icon thingy
-    def makeIcon(self, path):
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        return icon
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap(":/icons/Play.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.play.setIcon(icon)
 
-    def nextSong(self):
-        print("play next")
-
-    def getPlaylist(self):
-        self.playlist = QMediaPlaylist()
-        url = QtCore.QUrl.fromLocalFile("./Music/song.mp3")
-        self.playlist.addMedia(QMediaContent(url))
-        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
-        print(self.playlist.mediaCount())
-
+# does stuff
 if __name__ == "__main__":
         app = QtWidgets.QApplication(sys.argv)
         win = MainWindow()
